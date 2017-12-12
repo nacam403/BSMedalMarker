2921898b582f73d9a6b39ba6cee455ec19767547
nacam403 <satnakam@gmail.com>
2017-12-12 23:28:50 +0900

    Persist Redux store by redux-persist

--- index.js
+++ index.js
@@ -2,15 +2,29 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/es/storage';
+import { PersistGate } from 'redux-persist/es/integration/react';
 
 import reducers from './reducers';
 import App from './App';
 
-const store = createStore(reducers);
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['scene'],
+  debug: true
+};
+
+const reducer = persistReducer(persistConfig, reducers);
+const store = createStore(reducer);
+const persistor = persistStore(store);
 
 const Component = () => (
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
 
